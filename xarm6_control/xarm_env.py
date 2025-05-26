102f085193a2053744489119808f70bcca38b311
D-pad25 <daniel.padbury@gmail.com>
2025-05-26 17:17:03 +1000

    back again, removed clip and sending commands

--- xarm6_control/xarm_env.py
+++ xarm6_control/xarm_env.py
@@ -72,7 +72,7 @@ class XArmRealEnv:
         joint_action_deg = np.round(np.degrees(joint_action), 2)
         print(f"[STEP] Joint action: {joint_action}, Gripper action: {gripper_action}")
         print(f"[STEP] Joint action (deg): {joint_action_deg}, Gripper action: {gripper_action:.3f}")
-        # self.arm.set_servo_angle_j(joint_action, is_radian=True, wait=False)
+        self.arm.set_servo_angle_j(joint_action, is_radian=True, wait=False)
         # gripper_mm = 800 + gripper_action * (0 - 800)
         # self.arm.set_gripper_position(gripper_mm, wait=False)
 
