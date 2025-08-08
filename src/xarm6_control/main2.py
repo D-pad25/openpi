75ca37880482d0dd0f632fbfcfed0af25922498b
D-pad25 <daniel.padbury@gmail.com>
2025-08-08 15:19:49 +1000

    v4

--- src/xarm6_control/main2.py
+++ src/xarm6_control/main2.py
@@ -33,7 +33,7 @@ def main(
     step_through_instructions: bool = False,  # New argument
     delta_threshold: float = 0.25,  # New argument for delta threshold
     # log_dir: str = "/media/acrv/DanielsSSD/VLA_data",
-    log_dir: str = os.path.expanduser("~/Thesis/attention_maps/v3"),
+    log_dir: str = os.path.expanduser("~/Thesis/attention_maps/v4"),
     # log_dir: str = os.path.expanduser("~/test_logs"),
     save: bool = False,  # New argument to control saving behavior
     plot_attention: bool = False,  # New argument to control attention map plotting
