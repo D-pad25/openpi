0459054565f897b82c24f88d5c169bd81124d9e2
D-pad25 <daniel.padbury@gmail.com>
2025-08-08 11:48:39 +1000

    silly mistake

--- src/xarm6_control/main2.py
+++ src/xarm6_control/main2.py
@@ -89,8 +89,8 @@ def main(
                 base_rgb = np.array(Image.open(base_img_path).convert("RGB"))
                 wrist_rgb = np.array(Image.open(wrist_img_path).convert("RGB"))
                 # Resize with pad to match policy input size
-                base_rgb = image_tools.resize_with_pad(obs["base_rgb"], 224, 224)
-                wrist_rgb = image_tools.resize_with_pad(obs["wrist_rgb"], 224, 224)
+                base_rgb = image_tools.resize_with_pad(base_rgb, 224, 224)
+                wrist_rgb = image_tools.resize_with_pad(wrist_rgb, 224, 224)
             else:
                 base_rgb = image_tools.resize_with_pad(obs["base_rgb"], 224, 224)
                 wrist_rgb = image_tools.resize_with_pad(obs["wrist_rgb"], 224, 224)
