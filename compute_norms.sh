1a1984dd209d1927427c193668d2c2ba5c84a101
D-pad25 <daniel.padbury@gmail.com>
2025-08-31 12:34:22 +1000

    updated bash

--- compute_norms.sh
+++ compute_norms.sh
@@ -8,3 +8,6 @@
 
 # ─── Update enviroment path to cache direcotry  ───────────────────────────
  export OPENPI_DATA_HOME=$HOME/.cache/openpi
+
+# ─── Run the compute_norm_stats.py script ─────────────────────────────────
+ uv run scripts/compute_norm_stats.py --config-name pi0base_lora_xarm6_round2_fulldataset
