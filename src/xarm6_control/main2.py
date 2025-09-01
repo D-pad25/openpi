382c2e4547e7429b8fdcd54da5daed4f9e20b8c0
D-pad25 <daniel.padbury@gmail.com>
2025-09-01 10:47:52 +1000

    updated save location

--- src/xarm6_control/main2.py
+++ src/xarm6_control/main2.py
@@ -27,7 +27,7 @@ def main(
     control_hz: float = 30.0,  # ← New parameter: control frequency in Hz
     step_through_instructions: bool = False,  # New argument
     delta_threshold: float = 0.25,  # New argument for delta threshold
-    log_dir: str = "/media/acrv/DanielsSSD/VLA_data",
+    log_dir: str = "/media/acrv/DanielsSSD/Test_sem2",
     # log_dir: str = os.path.expanduser("~/test_logs"),
     save: bool = False,  # New argument to control saving behavior
 ):
