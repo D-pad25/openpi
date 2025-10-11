aabe1ad25da65e4c62f68b936e6e5dc7beda2bf1
D-pad25 <daniel.padbury@gmail.com>
2025-10-12 09:37:26 +1000

    amend

--- test_data.py
+++ test_data.py
@@ -1,7 +1,15 @@
 import tensorflow_datasets as tfds
-ds = tfds.load("agrivla_dataset_v3", data_dir="/home/n10813934/data/tfds_datasets", split="train")
+
+builder = tfds.builder("agrivla_dataset_v3", data_dir="/home/n10813934/data/tfds_datasets")
+label_names = builder.info.features["steps"].feature["observation"]["crop_type"].names
+print("Label names:", label_names)
+
+ds = builder.as_dataset(split="train")
 crops = []
+
 for ep in ds:
     s0 = next(ep["steps"].take(1).as_numpy_iterator())
-    crops.append(s0["observation"].get("crop_type", b"").decode("utf-8"))
-print(set(crops))
+    crop_id = s0["observation"]["crop_type"]  # integer
+    crops.append(label_names[int(crop_id)])
+
+print("Unique crops found:", set(crops))
